64b7af6a1f5fb13b29168c0928729d07d4d6b34d
DaiWenqi <361904805@qq.com>
2017-05-04 16:53:37 +0800

    commit

---
+++ src/main/java/com/feiniu/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.feiniu.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("test")
+public class IndexController {
+	@RequestMapping("/index")
+	public String list(HttpServletRequest request) {
+		return "index";
+	}
+}
